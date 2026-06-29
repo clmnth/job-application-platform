3d3b54204bdb74d809463c487a8b74364ee064f3
clmnt <huvigclement@gmail.com>
2026-06-29 16:31:38 +0700

    feat: add Features and SuccessStory sections to the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Features } from "@/components/homepage/Features";
 import { Hero } from "@/components/homepage/Hero";
 import { HowItWorks } from "@/components/homepage/HowItWorks";
+import { SuccessStory } from "@/components/homepage/SuccessStory";
 import { Navbar } from "@/components/layout/Navbar";
+
+const dividerClassName =
+  "landing-divider mx-auto h-10 max-w-[1440px] border-x border-border bg-surface";
 
 function Home() {
   return (
@@ -9,6 +14,14 @@ function Home() {
       <main className="pb-0">
         <Hero />
         <HowItWorks />
+        <div className="px-4 sm:px-6 lg:px-8">
+          <div className={dividerClassName} />
+        </div>
+        <Features />
+        <div className="px-4 sm:px-6 lg:px-8">
+          <div className={dividerClassName} />
+        </div>
+        <SuccessStory />
       </main>
     </>
   );
